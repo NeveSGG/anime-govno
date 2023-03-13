30f38d646bd90b5814189bebfcecf04053030058
NeveSGG <neverovsg@gmail.com>
2023-03-13 22:54:14 +0600

    feat: project initialized

---
+++ src/routes/app/App.tsx
@@ -0,0 +1,11 @@
+import { FC, useState } from 'react'
+
+const App: FC = () => {
+  return (
+    <div className="m-5 h-[448px] w-60 outline-dashed outline-2 outline-zinc-300 rounded-md ">
+      aabc
+    </div>
+  )
+}
+
+export default App
